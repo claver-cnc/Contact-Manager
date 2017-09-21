6a406e4b4146853eedd314cc731dd2d723c2741d
claver_cnc <claver.coulibaly@meltwater.org>
2017-09-21 18:26:26 +0000

    Update to python 3

--- contact.py
+++ contact.py
@@ -9,19 +9,19 @@ class contact:
         self.PostalAdress = PostalAdress
 
     def viewcontact(self):
-        print "\nName= " + self.Name + " | Phone Number= " + self.PhoneNumber + " | Gender= " + self.Gender+" | Email= " + self.EmailAdress
+        print("\nName= " + self.Name + " | Phone Number= " + self.PhoneNumber + " | Gender= " + self.Gender+" | Email= " + self.EmailAdress)
 
 boucle = 0
 while boucle ==0:
-    name = raw_input('User name:')
-    phone = raw_input('User phone:')
-    gender = raw_input('User gender: ')
-    email = raw_input('User mail: ')
-    postal = raw_input('User Postal Adress: ')
+    name = input('User name:')
+    phone = input('User phone:')
+    gender = input('User gender: ')
+    email = input('User mail: ')
+    postal = input('User Postal Adress: ')
     me = contact(name, phone, gender, email, postal)
     me.viewcontact()
 
-    answer = raw_input("\nWould you want to had one contact ? Yes || No\n")
+    answer = input("\nWould you want to had one contact ? Yes || No\n")
     if answer != 'Yes':
         boucle = 1
 
--- contactManager.py
+++ contactManager.py
@@ -12,66 +12,70 @@ class ContactManager:
         self.contacts = contacts
 
     def addContact(self):
-        myName = raw_input("\nEnter the name of your contact: ")
-        myNum = raw_input("Enter the phone number of your contact: ")
-        myMail = raw_input("Enter the email of this person: ")
+        myName = input("\nEnter the name of your contact: ")
+        myNum = input("Enter the phone number of your contact: ")
+        myMail = input("Enter the email of this person: ")
         self.contacts.append(Contact(myName, myNum, myMail))
 
     def delete(self):
-        toDelete = raw_input("\n Enter the name of contact your want to delete: ")
+        toDelete = input("\n Enter the name of contact your want to delete: ")
         for listContact in self.contacts:
             if listContact.name == toDelete:
                 self.contacts.remove(listContact)
-                print ("\n Contact Delected")
+                print("\n Contact Delected")
         return None
 
     def search(self):
-        toSearch = raw_input("\n Enter the name of your want to search: ")
+        toSearch = input("\n Enter the name of your want to search: ")
         found = 0
         for listContact in self.contacts:
             if listContact.name == toSearch:
-                print "Name= " + listContact.name + " | Contact= " + listContact.phoneNumber + " | Email= " + listContact.email
+                print("Name= " + listContact.name + " | Contact= " + listContact.phoneNumber + " | Email= " + listContact.email)
                 found = 1
         if found == 0:
-            print ("Sorry this contact doesn't existed")
+            print("Sorry this contact doesn't existed")
 
 
 boucle = 0
-print "      \n         Welcome Guy !! \n" \
-      "\nEnter the number of your operation " \
+print("      \n         Welcome Guy !! \n" \
+      "\nEnter the number of your operation " )
 
 while boucle == 0:
 
-    print "\n1- Add one number " \
+    print("\n1- Add one number " \
           "\n2- Delete one number " \
           "\n3- Search a contact " \
           "\n4- Show all the contacts" \
-          "\n5- Exit"
+          "\n5- Exit")
 
     choice = input()
     newcontact = ContactManager()
 
-    if choice == 1:
+    if choice == "1":
         add = 0
         while add == 0:
             newcontact.addContact()
-            answer = raw_input("Would you want to add a new contact ? Yes || No \n")
+            fiche = open("contact.txt", "a")
+            for listContact in newcontact.contacts:
+                fiche.write("Name= "+listContact.name+" | Contact= "+listContact.phoneNumber + " | Email= "+listContact.email+"\n")
+            fiche.close()
+            answer = input("Would you want to add a new contact ? Yes || No \n")
             if answer != "Yes":
                 add = 1
 
-    elif choice == 2:
+    elif choice == "2":
         newcontact.delete()
 
-    elif choice == 3:
+    elif choice == "3":
         newcontact.search()
 
-    elif choice == 4:
+    elif choice == "4":
         for listContact in newcontact.contacts:
-            print "Name= "+listContact.name+" | Contact= "+listContact.phoneNumber + " | Email= "+listContact.email
+            print("Name= "+listContact.name+" | Contact= "+listContact.phoneNumber + " | Email= "+listContact.email)
 
-    elif choice == 5:
+    elif choice == "5":
         boucle = 1
 
     else:
-        print "Make one real choice"
+        print("Make one real choice")
 
